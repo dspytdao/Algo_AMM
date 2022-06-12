8b2668deab3de5cce553aeee0ac321741e6cef10
pfed-prog <fedorovn19@gmail.com>
2022-06-12 22:20:41 +0300

    create asset added

--- README.md
+++ README.md
@@ -6,4 +6,10 @@
 pip install virtualenv
 virtualenv venv
 source venv/bin/activate
-pip install -r requirements.txt```
+pip install -r requirements.txt
+```
+
+## Useful Resources 
+
+[Testnet Dispensary](https://dispenser.testnet.aws.algodev.network/)
+[Py-algorand-sdk](https://py-algorand-sdk.readthedocs.io/en/latest/index.html)
---
+++ amm/create_asset.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import load_dotenv
+from algosdk import account
+from algosdk.future import transaction
+from algosdk.v2client import algod
+from algosdk import mnemonic
+load_dotenv()
+
+algod_address = "https://testnet-algorand.api.purestake.io/ps2"
+algod_token = os.getenv('algod_token')
+headers = {
+   "X-API-Key": algod_token,
+}
+
+# initialize an algodClient
+algod_client = algod.AlgodClient(algod_token, algod_address, headers)
+
+
+private_key = os.getenv('key')
+
+def wait_for_confirmation(client, txid):
+    last_round = client.status().get("last-round")
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get("confirmed-round") and txinfo.get("confirmed-round") > 0):
+        print("Waiting for confirmation...")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print(
+        "Transaction {} confirmed in round {}.".format(
+            txid, txinfo.get("confirmed-round")
+        )
+    )
+    return txinfo
+
+
+def create_asset(client, private_key):
+    # declare sender
+    sender = account.address_from_private_key(private_key)
+
+    params = client.suggested_params()
+
+    txn = transaction.AssetConfigTxn(
+        sender=sender,
+        sp=params,
+        total=1_000_000_000,
+        default_frozen=False,
+        unit_name="Copio",
+        asset_name="Cocoin",
+        manager=sender,
+        reserve=sender,
+        freeze=sender,
+        clawback=sender,
+        strict_empty_address_check=False,
+        url=None, 
+        decimals=0)
+
+    # Sign with secret key of creator
+    stxn = txn.sign(private_key)
+
+    # Send the transaction to the network and retrieve the txid.
+    
+    txid = client.send_transaction(stxn)
+    print("Signed transaction with txID: {}".format(txid))
+    # Wait for the transaction to be confirmed
+    confirmed_txn = wait_for_confirmation(client, txid)  
+    print("TXID: ", txid)
+    print("Result confirmed in round: {}".format(confirmed_txn['confirmed-round']))  
+
+create_asset(algod_client, private_key)
