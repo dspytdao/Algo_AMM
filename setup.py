877146727cb6e2a533b03b1fd0ed0ffe6f81f960
pfed-prog <fedorovn19@gmail.com>
2022-09-22 05:59:41 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+""" for pip package publishing"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="algo_amm",
+    version="0.0.1",
+    author="pfedprog",
+    author_email="pfedprog@gmail.com",
+    description="Automated Prediction Market Maker on Algorand",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dspytdao/Algo_AMM",
+    packages=setuptools.find_packages(),
+    install_requires = ['pyteal', 'py-algorand-sdk'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
